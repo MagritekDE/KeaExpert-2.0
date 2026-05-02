303ee059ac698e9f4164bcce7b5e2320f0aeab5d
craig <craig@magritek.com>
2026-05-02 19:49:09 +1200

    Batch files to make the Python environment and start the ExpertController interface.

---
+++ Python/makePyEnv.bat
@@ -0,0 +1,6 @@
+call Python\python.exe -m venv pyEnv
+call pyEnv\Scripts\activate.bat
+call pip install numpy
+call pip install matplotlib
+call pip install pyqt5
+call pip install pywin32
---
+++ Python/startExpertController.bat
@@ -0,0 +1,4 @@
+pushd %CD%\ExpertController
+call ..\pyEnv\Scripts\activate.bat
+call python.exe ExpertController.py
+pause
