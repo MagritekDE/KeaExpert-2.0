8b08729f62ccf951c781826b7600b0c35346ee38
craig <craig@magritek.com>
2026-08-10 20:31:48 +1200

    IsFileCached now accepts only 1 argument as an option - the macro name . In this case the macro path is returned if a match is found. Replaced tabs with spaces. GetProcedure is now accessible from DLLs

--- Code/Main Source Code/Source Code/cache.h
+++ Code/Main Source Code/Source Code/cache.h
@@ -1,6 +1,9 @@
 #ifndef CACHE_H
 #define CACHE_H
 
+#define EXPORT extern "C" __declspec(dllexport)
+
+
 class Interface;
 class Variable;
 class CText;
@@ -8,13 +11,13 @@ class CText;
 // Prospa command macros
 int CacheMacro(Interface* itfc, char args[]);
 int CacheProcedures(Interface* itfc, char args[]);
-int RemoveCachedMacro(Interface *itfc,  char args[]);
-int RemoveCachedMacros(Interface *itfc,  char args[]);
+int RemoveCachedMacro(Interface* itfc, char args[]);
+int RemoveCachedMacros(Interface* itfc, char args[]);
 int IsFileCached(Interface* itfc, char args[]);
 
 // Worker macros
-void AddProcedures(char *path, char* name);
-Variable* GetProcedure(char *path, char *macro, char *name);
+void AddProcedures(char* path, char* name);
+EXPORT Variable* GetProcedure(char* path, char* macro, char* name);
 void ListGlobalsCachedProcs(CText);
 
 // Global variables
